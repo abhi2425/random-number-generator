60c5bc1d812b34500cc0c599d49a1f6076b7f4d9
Abhinav_Jha <abhi.mount.26@gmail.com>
2022-06-11 20:00:01 +0530

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http')
+
+const port = process.env.PORT | 8080
+http
+  .createServer((req, res) => {
+    const url = req.url
+    const digits = url.split('/')?.[1]
+    const data = {
+      number: Math.floor(Math.random() * 10 ** digits),
+    }
+    res.end(JSON.stringify(data))
+  })
+  .listen(port)
