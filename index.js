7a7af25323dbb074feab18461ec4e4f0e85ad55f
Abhinav Jha <abhi.mount.26@gmail.com>
2022-09-25 19:46:49 +0530

    fixed a bug for examples like four digit random number and Math.random() generates 0.0763 => 763

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const port = process.env.PORT || 8080
 http
   .createServer((req, res) => {
     const url = req.url
-    const digits = url.split('/')?.[1]
+    const digits = +url.split('/')?.[1]
+    let random = Math.random()
+    while(random<= 0.10) random= Math.random()
     const data = {
-      number: Math.floor(Math.random() * 10 ** digits),
+      number: Math.floor(random * 10 ** digits)
     }
     res.end(JSON.stringify(data))
   })
